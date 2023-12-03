1875123fceebfd9e2a8196b01b319e2a049868c1
chewbacca234 <carine.dupuis.es@gmail.com>
2023-12-03 18:15:31 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# morningnews_backend
+
+Backend part of the web application Morning News
---
+++ models/connection.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const connectionString = process.env.CONNECTION_STRING;
+
+mongoose.set('strictQuery', false);
+
+mongoose
+  .connect(connectionString, { connectTimeoutMS: 2000 })
+  .then(() => console.log('[DATABASE] connected'))
+  .catch(error => console.error(error));
