c03a2ffd19aab42fffcad2e323167f2966fc6ea4
chewbacca234 <carine.dupuis.es@gmail.com>
2023-12-07 21:27:51 +0100

    remove fetch require

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,6 @@
 var express = require('express');
 var router = express.Router();
 
-const fetch = require('node-fetch');
-
 const NEWS_API_KEY = process.env.NEWS_API_KEY;
 
 /* GET the tech sources top headlines in inglish */
@@ -27,14 +25,26 @@ router.get('/sources', (_, res) => {
 });
 
 /* GET the tech articles from selected sources */
-router.get('/articles', (_, res) => {
+router.get('/articles/:selectedSources', (req, res) => {
+  const { selectedSources } = req.params;
+
   fetch(
-    `https://newsapi.org/v2/top-headlines?sources=the-verge&apiKey=${NEWS_API_KEY}`
+    `https://newsapi.org/v2/top-headlines?sources=${selectedSources}&apiKey=${NEWS_API_KEY}`
   )
     .then(response => response.json())
     .then(data => {
       if (data.status === 'ok') {
-        return res.json({ articles: data.articles });
+        const articles = [];
+        const regex = /^https:\/\//i;
+
+        data.articles.map(article => {
+          if (!regex.test(article.urlToImage)) {
+            article.urlToImage = null;
+          }
+          articles.push(article);
+        });
+
+        return res.json({ articles });
       } else {
         return res.json({ articles: [] });
       }
