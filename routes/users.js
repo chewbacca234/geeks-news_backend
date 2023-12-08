842e8283c0ba79a4975425819a0768d106b96dec
chewbacca234 <carine.dupuis.es@gmail.com>
2023-12-08 17:04:32 +0100

    add comments

--- routes/users.js
+++ routes/users.js
@@ -7,7 +7,9 @@ const { checkBody } = require('../modules/checkBody');
 const uid2 = require('uid2');
 const bcrypt = require('bcrypt');
 
+/* POST Register new user */
 router.post('/signup', (req, res) => {
+  // Check if sended datas are correct
   if (!checkBody(req.body, ['username', 'password'])) {
     return res.json({ result: false, error: 'Missing or empty fields' });
   }
@@ -15,6 +17,7 @@ router.post('/signup', (req, res) => {
   // Check if the user has not already been registered
   User.findOne({ username: req.body.username }).then(data => {
     if (data === null) {
+      // User doesn't exists in database
       const hash = bcrypt.hashSync(req.body.password, 10);
 
       const newUser = new User({
@@ -24,6 +27,7 @@ router.post('/signup', (req, res) => {
         canBookmark: true,
       });
 
+      // If all ok save new user & return username & token
       newUser.save().then(newDoc => {
         return res.json({
           result: true,
@@ -32,25 +36,31 @@ router.post('/signup', (req, res) => {
         });
       });
     } else {
-      // User already exists in database
+      // If user already exists in database return error message
       return res.json({ result: false, error: 'User already exists' });
     }
   });
 });
 
+/* POST Connect existing user */
 router.post('/signin', (req, res) => {
+  // Check if sended datas are correct
   if (!checkBody(req.body, ['username', 'password'])) {
     return res.json({ result: false, error: 'Missing or empty fields' });
   }
 
+  // Check if the user exists in Db
   User.findOne({ username: req.body.username }).then(data => {
+    // Check if the password is correct
     if (data && bcrypt.compareSync(req.body.password, data.password)) {
+      // If all ok return username & token
       return res.json({
         result: true,
         username: data.username,
         token: data.token,
       });
     } else {
+      // If user doesn't exists in database or wrong password return error message
       return res.json({
         result: false,
         error: 'User not found or wrong password',
@@ -59,11 +69,14 @@ router.post('/signin', (req, res) => {
   });
 });
 
+/* GET User can bookmark status */
 router.get('/canBookmark/:token', (req, res) => {
   User.findOne({ token: req.params.token }).then(data => {
     if (data) {
+      // If user exists return the can bookmark boolean
       return res.json({ result: true, canBookmark: data.canBookmark });
     } else {
+      // If user doesn't exists in database return error message
       return res.json({ result: false, error: 'User not found' });
     }
   });
